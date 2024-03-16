50deb07b748c7a1790b377b380de6c6dbf2ba41d
kakakhai <kaisakai15@gmail.com>
2024-03-17 04:18:36 +0700

    add urlencoded

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const bodyParser = require("body-parser")
 const app = express()
-const port = process.env.PORT || 5001
-
 app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 5001
 
 app.get('/', (req, res) => {
     console.log("/")
@@ -12,6 +13,10 @@ app.get('/', (req, res) => {
 
 app.post("/hook", (req, res) => {
     console.log("hook", req.body) // Call your action on the request here
+    console.log("-------------- New Request --------------");
+    console.log("Headers:"+ JSON.stringify(req.headers, null, 3));
+    console.log("Body:"+ JSON.stringify(req.body, null, 3));
+    res.json({ message: "Thank you for the message" });
     res.status(200).end() // Responding is important
 })
 
